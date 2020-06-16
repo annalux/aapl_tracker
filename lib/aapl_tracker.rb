2e61294b48b1dcd2b759656514ec6bc33817f6c7
github username <anna.watt.lux@gmail.com>
2020-06-16 09:10:57 -0400

    add executable aapl_tracker file

---
+++ lib/aapl_tracker.rb
@@ -0,0 +1,6 @@
+require "aapl_tracker/version"
+
+module AaplTracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/aapl_tracker/version.rb
@@ -0,0 +1,3 @@
+module AaplTracker
+  VERSION = "0.1.0"
+end
